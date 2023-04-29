7f17a459ce574d5be8eb7f54295ae316467a2a9f
William George <42682665+wgeorge1990@users.noreply.github.com>
2023-04-29 17:30:11 -0400

    ADD: Profile model schema and working relationship to User model

--- db/migrate/20230429211230_create_profiles.rb
+++ db/migrate/20230429211230_create_profiles.rb
@@ -2,7 +2,20 @@ class CreateProfiles < ActiveRecord::Migration[7.0]
   def change
     create_table :profiles do |t|
       t.bigint  :user_id
-
+      t.string :first_name
+      t.string :middle_name
+      t.string :last_name
+      t.string :bio
+      t.string :profile_image
+      t.datetime :logged_in_at
+      t.datetime :logged_out_at
+      t.boolean :active
+      t.date :birthday
+      t.integer :age
+      t.string :headline
+      t.integer :followers
+      t.integer :following
+      
       t.timestamps
     end
     add_index :profiles, :user_id
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_29_202916) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_29_211230) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -19,6 +19,26 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_29_202916) do
     t.integer "followed_user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "profiles", force: :cascade do |t|
+    t.bigint "user_id"
+    t.string "first_name"
+    t.string "middle_name"
+    t.string "last_name"
+    t.string "bio"
+    t.string "profile_image"
+    t.datetime "logged_in_at"
+    t.datetime "logged_out_at"
+    t.boolean "active"
+    t.date "birthday"
+    t.integer "age"
+    t.string "headline"
+    t.integer "followers"
+    t.integer "following"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_profiles_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
