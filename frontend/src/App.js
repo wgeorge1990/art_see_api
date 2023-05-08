cc0c38682f6eec652c27145b229edbcf659b79cd
William George <42682665+wgeorge1990@users.noreply.github.com>
2023-05-08 00:21:41 -0400

    ADD: Homecontainer, NEXT: Add MainContent and SideBar to ProfilePage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import './App.css';
 
+import HomeContainer from "./components/HomeContainer/HomeContainer.jsx";
+// import MainContent from "./components/MainContent/MainContent";
+// import Sidebar from "./components/Sidebar/Sidebar";
+
 import ProfileImageForm from './components/ProfileImageForm';
 
 export default function App() {
   return (
         <div className="App" >
           <ProfileImageForm />
+          <HomeContainer />
+          {/* <MainContent/> */}
+          {/* <Sidebar /> */}
         </div>
   )
 }
---
+++ frontend/src/components/HomeContainer/HomeContainer.jsx
@@ -0,0 +1,63 @@
+import styles from "../../styles/abstracts/styles.module.css";
+import "./homecontainer.sass";
+import arrow from "../assets/arrow.svg";
+import { Link } from "react-scroll";
+import useMeasure from "react-use-measure";
+import { useTrail, animated } from "@react-spring/web";
+import React from "react";
+
+const fast = { tension: 1200, friction: 40 };
+const slow = { mass: 10, tension: 200, friction: 50 };
+const trans = (x, y) =>
+  `translate3d(${x}px,${y}px,0) translate3d(-50%,-50%,0)`;
+
+const HomeContainer = () => {
+  const [trail, api] = useTrail(3, (i) => ({
+    xy: [0, 0],
+    config: i === 0 ? fast : slow,
+  }));
+  const [ref, { left, top }] = useMeasure();
+
+  const handleMouseMove = (e) => {
+    api.start({ xy: [e.clientX - left, e.clientY - top] });
+  };
+
+  const { innerWidth } = window;
+
+  return (
+    <div className="mainDiv">
+      <div className="mainItem">
+        <h1>Will George</h1>
+        <span>Full Stack Developer</span>
+      </div>
+      <div className="mainImg"></div>
+      <svg style={{ position: "absolute", width: 0, height: 0 }}>
+        <filter id="goo">
+          <feGaussianBlur in="SourceGraphic" result="blur" stdDeviation="30" />
+          <feColorMatrix
+            in="blur"
+            values="1 0 0 0 0 0 1 0 0 0 0 0 1 0 0 0 0 0 30 -7"
+          />
+        </filter>
+      </svg>
+      <div ref={ref} className={styles.hooksMain} onMouseMove={handleMouseMove}>
+        {trail.map((props, index) => (
+          <animated.div key={index} style={{ transform: props.xy.to(trans) }} />
+        ))}
+      </div>
+      <Link
+        className="arrow"
+        eslint-disable-next-line no-restricted-globals
+        to={innerWidth > 1000 ? "main-content" : "sidebar"}
+        spy={true}
+        smooth={true}
+        offset={-90}
+        duration={700}
+      >
+        <img src={arrow} alt="Seta" />
+      </Link>
+    </div>
+  );
+};
+
+export default HomeContainer;
